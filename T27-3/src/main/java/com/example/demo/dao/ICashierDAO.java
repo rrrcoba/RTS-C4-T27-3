4538313cbb237e1bd9a8de39cfecdfa2968b5ae9
Roger <108723939+rrrcoba@users.noreply.github.com>
2022-08-22 11:33:18 +0200

    Delete T27-3 directory

--- T27-3/src/main/java/com/example/demo/dao/ICashierDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.dto.Cashier;
-
-public interface ICashierDAO extends JpaRepository<Cashier, Long>{
-
-}
